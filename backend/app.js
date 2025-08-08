8e2851b574314af923eb8dbf5fbe1d2e652c1eed
siddharth kumar rai <siddharthkumarrai777@gmail.com>
2025-08-08 23:54:38 +0530

    fix cors error

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({extended: true}))
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    origin: process.env.FRONTEND_URL|| 'http://localhost:5173/',
     credentials: true
 }));
 
